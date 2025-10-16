d855a2d960020e4985cb9f27519123764586a313
Faustine09 <faustine.richardgauguin@gmail.com>
2025-10-16 11:06:29 +0200

    base

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
+class calculette():
+
+    def __init__(self):
+        pass
+
+    def addition(self):
+        pass
+
+    def soustraction(self):
+        pass
+
+    def mutliplication(self):
+        pass
+
+    def division(self):
+        pass
+
+    def exponentielle(self):
+        pass
+
+    def fibonacci(self):
+        pass
+
+    def nbpremier(self):
+        pass
+
+
 print("je teste des trucs")
